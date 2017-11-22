861277c9f6502a5f822b541ecefa54789f33c84a
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-11-22 14:56:28 +0700

    Add method and callbacks for Welcome Activity

--- app/src/main/java/com/a58070096/patcharaponjoksamut/steamstalker/Activity/WelcomeActivity.java
+++ app/src/main/java/com/a58070096/patcharaponjoksamut/steamstalker/Activity/WelcomeActivity.java
@@ -1,21 +1,40 @@
 package com.a58070096.patcharaponjoksamut.steamstalker.Activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.a58070096.patcharaponjoksamut.steamstalker.R;
+import com.a58070096.patcharaponjoksamut.steamstalker.ViewModel.AuthenticationViewModel;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements AuthenticationViewModel.AuthenticationCallbacks {
+
+    AuthenticationViewModel authenticationViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        initalizeVariables();
+
+
     }
 
+    private void initalizeVariables() {
+        this.authenticationViewModel = new AuthenticationViewModel();
+    }
+
+    public void onGetStartedButtonPressed(View view) {
+    }
+
+    @Override
+    public void onAuthenticationSuccessful() {
+        //TODO: Back to Home Activity
+    }
+
+    @Override
+    public void onAuthenticationFailure() {
+        //TODO: Show Error
+    }
 }
---
+++ app/src/main/java/com/a58070096/patcharaponjoksamut/steamstalker/ViewModel/AuthenticationViewModel.java
@@ -0,0 +1,18 @@
+package com.a58070096.patcharaponjoksamut.steamstalker.ViewModel;
+
+/**
+ * Created by patcharaponjoksamut on 22/11/2017 AD.
+ */
+
+public class AuthenticationViewModel {
+
+    public interface AuthenticationCallbacks {
+        void onAuthenticationSuccessful();
+        void onAuthenticationFailure();
+    }
+
+    public void authenticate() {
+
+    }
+
+}
