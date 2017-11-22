12c9e2bb3d99349d4f5ff39e6f79fdbce6330e44
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-11-22 17:21:34 +0700

    Change Bottom Navigation To external liblary

--- app/src/main/java/com/a58070096/patcharaponjoksamut/steamstalker/Activity/HomeActivity.java
+++ app/src/main/java/com/a58070096/patcharaponjoksamut/steamstalker/Activity/HomeActivity.java
@@ -1,22 +1,37 @@
 package com.a58070096.patcharaponjoksamut.steamstalker.Activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.a58070096.patcharaponjoksamut.steamstalker.R;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_navigation)
+    AHBottomNavigation bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        ButterKnife.bind(this);
+
         getSupportActionBar().hide();
 
+        this.initializeBottomNavigationBar();
         this.checkForLogin();
     }
 
@@ -30,5 +45,33 @@ public class HomeActivity extends AppCompatActivity {
         if(user == null) {
             this.showWelcomeActivity();
         }
+    }
+
+    private void initializeBottomNavigationBar() {
+        AHBottomNavigationItem newsItem = new AHBottomNavigationItem(R.string.text_web, R.drawable.ic_web_black_24dp, R.color.white);
+        AHBottomNavigationItem gameItem = new AHBottomNavigationItem(R.string.text_videogame, R.drawable.ic_videogame_asset_black_24dp, R.color.white);
+        AHBottomNavigationItem profileItem = new AHBottomNavigationItem(R.string.text_person, R.drawable.ic_person_black_24dp, R.color.white);
+
+        bottomNavigationView.addItem(newsItem);
+        bottomNavigationView.addItem(gameItem);
+        bottomNavigationView.addItem(profileItem);
+
+        bottomNavigationView.setDefaultBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+        bottomNavigationView.setBehaviorTranslationEnabled(false);
+        bottomNavigationView.setAccentColor(getResources().getColor(R.color.white));
+        bottomNavigationView.setInactiveColor(getResources().getColor(R.color.colorPrimary));
+        bottomNavigationView.setForceTint(true);
+        bottomNavigationView.setTranslucentNavigationEnabled(true);
+
+        bottomNavigationView.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
+
+        bottomNavigationView.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+
+                return true;
+            }
+        });
+
     }
 }
