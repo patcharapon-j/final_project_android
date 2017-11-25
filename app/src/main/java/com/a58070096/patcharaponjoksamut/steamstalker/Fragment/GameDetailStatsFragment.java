01b2b91b2a3973e95741281241bb23eaf5a8bc10
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-11-25 13:36:30 +0700

    Add Fragment For Game Details

---
+++ app/src/main/java/com/a58070096/patcharaponjoksamut/steamstalker/Fragment/GameDetailStatsFragment.java
@@ -0,0 +1,30 @@
+package com.a58070096.patcharaponjoksamut.steamstalker.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.a58070096.patcharaponjoksamut.steamstalker.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GameDetailStatsFragment extends Fragment {
+
+
+    public GameDetailStatsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_detail_stats, container, false);
+    }
+
+}
