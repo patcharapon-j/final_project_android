2d3e2346a01a838b306bf0af041d74bdc9d3e2ec
Patcharapon Joksamut <vampirefeem@gmail.com>
2017-11-30 17:28:38 +0700

    initialize file

---
+++ app/src/test/java/com/a58070096/patcharaponjoksamut/steamstalker/ViewModel/GameDetailInfoViewModelTest.java
@@ -0,0 +1,16 @@
+package com.a58070096.patcharaponjoksamut.steamstalker.ViewModel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by patcharaponjoksamut on 30/11/2017 AD.
+ */
+public class GameDetailInfoViewModelTest {
+    @Test
+    public void getGameName() throws Exception {
+
+    }
+    
+}
